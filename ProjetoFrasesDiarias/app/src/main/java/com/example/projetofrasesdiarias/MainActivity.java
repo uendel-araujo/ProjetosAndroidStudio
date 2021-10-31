ea173901014c6e1471302b490d62ae45e7b37ac0
Uendel Araujo <dendellaraujo@gmail.com>
2021-10-31 17:03:28 -0300

    Finalização da adição das frases

--- ProjetoFrasesDiarias/app/src/main/java/com/example/projetofrasesdiarias/MainActivity.java
+++ ProjetoFrasesDiarias/app/src/main/java/com/example/projetofrasesdiarias/MainActivity.java
@@ -20,7 +20,14 @@ public class MainActivity extends AppCompatActivity {
                 "Dificuldades preparam pessoas comuns para destinos extraordinários.",
                 "Você é mais forte e capaz do que imagina. Acredite!",
                 "Para todo game over (FIM DE JOGO), existe um play again (JOGAR NOVAMENTE).",
-                
+                "Por maior que seja, não há obstáculo que não possa ser superado com fé, esperança e persistência.",
+                "Ontem uma pessoa me disse que superação não é sentir nada, pelo contrário, é sentir algo pela pessoa e mesmo assim não correr atrás.",
+                "Você superou tantas coisas, não desista, por favor!",
+                "Na vida, ter fé não significa que tudo dará certo, e sim que tudo pode ser superado.",
+                "Na vida, ter fé não significa que tudo dará certo, e sim que tudo pode ser superado.",
+                "Se você está lendo isso, eu espero que você encontre a força que você precisa para superar o que está te causando tanto conflito interno.",
+                "Você se surpreende ao perceber o quanto pode suportar."
+
         };
 
         TextView texto = findViewById(R.id.textResultado);

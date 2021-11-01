18fe3d37b246df5c78f312716c26c959ca858cf0
Uendel Araujo <dendellaraujo@gmail.com>
2021-11-01 19:23:23 -0300

    Criação do numero random. criação do array com as opcoes pedra papel e tesoura e criação do switch para definir a escolha do computador

--- ProjetoPedraPapelTesoura/app/src/main/java/com/example/projetopedrapapeltesoura/MainActivity.java
+++ ProjetoPedraPapelTesoura/app/src/main/java/com/example/projetopedrapapeltesoura/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.projetopedrapapeltesoura;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +28,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void opcaoSelecionada(String opcaoSelecionada){
-        System.out.println(opcaoSelecionada);
+
+        ImageView  imageOpcaoPC = findViewById(R.id.imageOpcaoPC);
+
+        int numero = new Random().nextInt(3);
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        String opcaoPC = opcoes[numero];
+
+        switch (opcaoPC) {
+            case "pedra":
+                imageOpcaoPC.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imageOpcaoPC.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imageOpcaoPC.setImageResource(R.drawable.tesoura);
+                break;
+
+
+      }
+
+      System.out.println(opcaoSelecionada);
     }
 
 }

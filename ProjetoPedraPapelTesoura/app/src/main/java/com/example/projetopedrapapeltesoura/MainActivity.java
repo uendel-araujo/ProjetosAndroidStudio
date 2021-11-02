8ec99158e3d01885bff9dc7b0a19241e54b53265
Uendel Araujo <dendellaraujo@gmail.com>
2021-11-01 22:24:57 -0300

    Finalização do APP e correção de um bug para entrada da condicional

--- ProjetoPedraPapelTesoura/app/src/main/java/com/example/projetopedrapapeltesoura/MainActivity.java
+++ ProjetoPedraPapelTesoura/app/src/main/java/com/example/projetopedrapapeltesoura/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.projetopedrapapeltesoura;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -16,20 +18,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void selecionadoPedra(View view){
-        this.opcaoSelecionada("Pedra");
+        this.opcaoSelecionada("pedra");
     }
 
     public void selecionadoPapel(View view) {
-        this.opcaoSelecionada("Papel");
+        this.opcaoSelecionada("papel");
     }
 
     public void selecionadoTesoura(View view) {
-        this.opcaoSelecionada("Tesoura");
+        this.opcaoSelecionada("tesoura");
     }
 
-    public void opcaoSelecionada(String opcaoSelecionada){
+    public void opcaoSelecionada(String opcaoUsuario){
 
         ImageView  imageOpcaoPC = findViewById(R.id.imageOpcaoPC);
+        TextView textoResultadoFinal = findViewById(R.id.textResultadoFinal);
 
         int numero = new Random().nextInt(3);
         String[] opcoes = {"pedra", "papel", "tesoura"};
@@ -45,11 +48,18 @@ public class MainActivity extends AppCompatActivity {
             case "tesoura":
                 imageOpcaoPC.setImageResource(R.drawable.tesoura);
                 break;
-
-
       }
 
-      System.out.println(opcaoSelecionada);
+      if((opcaoPC.equals("tesoura") && opcaoUsuario.equals("papel")) || (opcaoPC.equals("papel") && opcaoUsuario.equals("pedra")) || (opcaoPC.equals("pedra") && opcaoUsuario.equals("tesoura"))) {//POSSIBILIDADES DO APP GANHADOR
+                textoResultadoFinal.setText("Computador ganhou!!");
+      }
+      else if(( opcaoUsuario.equals("papel") && opcaoPC.equals("pedra")) || ( opcaoUsuario.equals("tesoura") && opcaoPC.equals("papel")) || ( opcaoUsuario.equals("pedra") && opcaoPC.equals("tesoura"))) {
+                textoResultadoFinal.setText("Usuário ganhou!!");
+      }
+      else
+      {
+          textoResultadoFinal.setText("Ninguém ganhou!! Jogo deu empate");
+      }
     }
 
 }

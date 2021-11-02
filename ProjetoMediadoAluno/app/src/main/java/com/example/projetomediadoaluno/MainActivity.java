767998a136de26da2cecd49a6a5e96e4fd47d278
Uendel Araujo <dendellaraujo@gmail.com>
2021-11-02 13:02:15 -0300

    Criado a interface do app e gerado os ID dos componentes

--- ProjetoMediadoAluno/app/src/main/java/com/example/projetomediadoaluno/MainActivity.java
+++ ProjetoMediadoAluno/app/src/main/java/com/example/projetomediadoaluno/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.projetomediadoaluno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void calcularMediaFinal(View view) {
+
+        EditText editMedia1 = findViewById(R.id.editMedia1);
+        double media1 = Double.parseDouble(editMedia1.getText().toString());
+
+        EditText editMedia2 = findViewById(R.id.editMedia2);
+        double media2 = Double.parseDouble(editMedia2.getText().toString());
+
+        EditText editMedia3 = findViewById(R.id.editMedia3);
+        double media3 = Double.parseDouble(editMedia3.getText().toString());
+
     }
 }

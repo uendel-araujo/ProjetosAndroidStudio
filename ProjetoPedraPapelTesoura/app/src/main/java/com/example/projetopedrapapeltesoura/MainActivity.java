e8c94f385f801a870161aea6bbc444a6964c0bd9
Uendel Araujo <dendellaraujo@gmail.com>
2021-11-01 23:53:06 -0300

    Projeto totalmente finalizado e realizado varias melhorias

--- ProjetoPedraPapelTesoura/app/src/main/java/com/example/projetopedrapapeltesoura/MainActivity.java
+++ ProjetoPedraPapelTesoura/app/src/main/java/com/example/projetopedrapapeltesoura/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    int placarUsuario = 0;
+    int placarComputador = 0;
+    int placarEmpate = 0;
+    int totalPartidas = 0;
+
 
     public void selecionadoPedra(View view){
         this.opcaoSelecionada("pedra");
@@ -33,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView  imageOpcaoPC = findViewById(R.id.imageOpcaoPC);
         TextView textoResultadoFinal = findViewById(R.id.textResultadoFinal);
+        TextView textResultadoUsuario = findViewById(R.id.textResultadoUsuario);
+        TextView textResultadoComputador = findViewById(R.id.textResultadoComputador);
+        TextView textResultadoEmpate    = findViewById(R.id.textResultadoEmpate);
+        TextView textPartidasTotalJogadas = findViewById(R.id.textPartidasTotalJogadas);
+
 
         int numero = new Random().nextInt(3);
         String[] opcoes = {"pedra", "papel", "tesoura"};
@@ -52,14 +66,24 @@ public class MainActivity extends AppCompatActivity {
 
       if((opcaoPC.equals("tesoura") && opcaoUsuario.equals("papel")) || (opcaoPC.equals("papel") && opcaoUsuario.equals("pedra")) || (opcaoPC.equals("pedra") && opcaoUsuario.equals("tesoura"))) {//POSSIBILIDADES DO APP GANHADOR
                 textoResultadoFinal.setText("Computador ganhou!!");
+                placarComputador++;
+                textResultadoComputador.setText(Integer.toString(placarComputador));
       }
       else if(( opcaoUsuario.equals("papel") && opcaoPC.equals("pedra")) || ( opcaoUsuario.equals("tesoura") && opcaoPC.equals("papel")) || ( opcaoUsuario.equals("pedra") && opcaoPC.equals("tesoura"))) {
                 textoResultadoFinal.setText("Usuário ganhou!!");
+                placarUsuario++;
+                textResultadoUsuario.setText(Integer.toString(placarUsuario));
       }
       else
       {
           textoResultadoFinal.setText("Ninguém ganhou!! Jogo deu empate");
+          placarEmpate++;
+          textResultadoEmpate.setText(Integer.toString(placarEmpate));
       }
+
+        totalPartidas = placarEmpate + placarComputador + placarUsuario;
+        textPartidasTotalJogadas.setText(Integer.toString(totalPartidas));
+
     }
 
 }
